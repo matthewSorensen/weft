e95ce2f376cd2c186a29dfbdd803a0d4d840d558
Matthew Sorensen <scholl.ma@gmail.com>
2011-08-20 16:43:40 -0700

    Making things line up with skeinforge output more

--- weft/Weft/Commands.hs
+++ weft/Weft/Commands.hs
@@ -19,32 +19,36 @@ absolutePosition = emit "G90"
 pause::Int->Print ()
 pause milis      = emit $ ap "G4 P" (bshow milis)
 
-waitForTemp::Int->Int->Print ()
-waitForTemp tool temp = do
-  emit $ con ["M104 S",bshow temp," T",bshow tool] -- Set the temp
-  emit $ ap "M6 T" (bshow tool) -- Wait for the tool to reach temp
-  
--- Should we set the location to (0,0,0) after this?
+waitForTemp::Int->Print ()
+waitForTemp tool = emit $ ap "M6 T" (bshow tool) 
+
 setCurrentHome::(Int,Int,Int)->Print ()
 setCurrentHome (x,y,z) = emit $ con ["G92 X",bshow x," Y",bshow y," Z",bshow z, " "]
 
-setToolSpeed::Int->Int->Print ()
-setToolSpeed tool speed = emit $ con ["M",bshow (1+tool),"08 S",bshow speed]
+setToolSpeed::Int->Double->Print ()
+setToolSpeed tool speed = emit $ con ["M",bshow (1+tool),"08 R",bshow speed]
 
 platformTemp::Int->Int->Print ()
 platformTemp tool temp = emit $ con ["M109 S",bshow temp," T",bshow tool]
 
-move3::Point3->Double->Print ()
+extruderTemp::Int->Int->Print ()
+extruderTemp tool temp =   emit $ con ["M104 S",bshow temp," T",bshow tool] 
+
+setFeedrate::Double->Print ()
+setFeedrate r = getPrinterState >>= setPrinterState . (\s-> s {feedrate = Just r})
+
+move3::Point3->Maybe Double->Print ()
 move3 p@(x,y,z) speed = do
+  feed <- normalizeFeedrate speed
   setLocation p
-  emit $ con ["G1 X",bshow x," Y",bshow y," Z",bshow z," F",bshow speed]  
+  emit $ con ["G1 X",bshow x," Y",bshow y," Z",bshow z," F",bshow feed]  
 -- Make a 2d movement on the same z-plane
-move2::Point2->Double->Print ()
+move2::Point2->Maybe Double->Print ()
 move2 (x,y) speed = do
   (_,_,z) <- getLocation
   move3 (x,y,z) speed
 -- move relative to the current location
-moveRelative::Point3->Double->Print ()
+moveRelative::Point3->Maybe Double->Print ()
 moveRelative (x,y,z) speed = do
   (x',y',z') <- getLocation
   move3 (x+x',y+y',z+z') speed
--- weft/Weft/Monad.hs
+++ weft/Weft/Monad.hs
@@ -7,7 +7,8 @@ import qualified Data.ByteString as B
 
 type ByteString = B.ByteString
 
-data PrinterState = PState {output::ByteString->IO ()}
+data PrinterState = PState {output::ByteString->IO (),
+                            feedrate::Maybe Double}
                
 type Point3 = (Double,Double,Double)
 type Point2 = (Double,Double)
@@ -30,5 +31,9 @@ emit::ByteString->Print ()
 emit b = getPrinterState >>= (\s-> liftIO $ (output s) b)
 
 runWithIOAction::(ByteString->IO ())->Print a->IO a
-runWithIOAction dest instr = let init = ((0,0,0), PState {output = dest})
+runWithIOAction dest instr = let init = ((0,0,0), PState {output = dest, feedrate = Nothing})
                              in evalStateT instr init
+-- Throws an error if it can't find any feedrate                                
+normalizeFeedrate::Maybe Double->Print Double
+normalizeFeedrate = maybe (fmap fromState getPrinterState) return
+  where fromState = maybe (error "No feedrate defined in printer state") id . feedrate
