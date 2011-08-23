6612d9f93b88bc06c44f11d0cf2f6a63bb681316
Matthew Sorensen <scholl.ma@gmail.com>
2011-08-23 00:39:33 -0700

    stab at that

--- weft/Weft/Geometry/Polygon.hs
+++ weft/Weft/Geometry/Polygon.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE TupleSections #-}
 module Weft.Geometry.Polygon where
 
 import Weft.Points 
-import Data.Vector hiding (concat,empty,reverse)
+import Data.Vector hiding (concat,empty,reverse,foldl,head)
 import Prelude hiding (map,scanl,tail)
 import Data.List (sortBy,reverse)
+import Data.Function (on)
 import Data.Set (Set,empty,insert,elems,delete)
 import qualified Data.List as L
 
@@ -14,19 +16,17 @@ type Range = (Double,Double)
 type Action = Int->Set Int->Set Int
 
 project::Point2->Polyline->[(Range,[Int])]
-project basis = overlap . sortBy order . tag . toIntervals basis
+project basis = reverse . overlap . sortBy order . tag . toIntervals basis
   where toIntervals i (Poly d p) = tail $ scanl (\(_,cur) len -> (cur,len+cur)) (0.0, i<.>d) $ map (i<.>) p
         tag = concat . toList . imap tagSegment
         tagSegment index (start,end) 
           | start == end = []
-          | otherwise    = [(insert,index,min start end),(delete,index,max start end)]
-        order (_,_,a) (_,_,b) = a `compare` b         
-        
-overlap::[(Action,Int,Double)]->[(Range,[Int])]
-overlap [] = []
-overlap x  = overlap' (init $ x !! 0) x
-  where init (_,_,x) = ([],empty,x)
-        overlap' (acc,_,_) [] = reverse acc
-        overlap' (acc,set,lower) ((action,index,upper):rest)
-          | lower == upper = overlap' (acc,action index set,lower) rest
-          | otherwise      = overlap' (((lower,upper),elems set):acc,action index set,upper) rest
+          | otherwise    = [(insert,(index,min start end)),(delete,(index,max start end))]
+        order = compare `on` (snd . snd)         
+        overlap [] = []
+        overlap x  = fst $ foldl overlap' (init x) x
+            where init = ([],) . (empty,) . snd . snd . head
+                  overlap' (acc,(set,lower)) (action,(index,upper))
+                    | lower == upper = (acc,(action index set,lower))
+                    | otherwise      = let seg = ((lower,upper),elems set)
+                                       in (seg:acc,(action index set,upper))
